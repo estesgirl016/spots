3d12bfef3a99fd95b7bab15fe2a3a18eee5f0093
Charles Brown <adsr13@gmail.com>
2016-09-29 17:07:56 -0600

    s3_protocol: http

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   # Sets images to upload to Amazon S3
   config.paperclip_defaults = {
     storage: :s3,
+    s3_protocol: 'http',
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
--- config/initializers/paperclip.rb
+++
@@ -1,2 +0,0 @@
-Paperclip::Attachment.default_options[:url] = ':https://biddle-spots.herokuapp.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:'
