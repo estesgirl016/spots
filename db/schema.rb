79a384e712a16462808a56ce3e9422005a38a59b
Charles Brown <adsr13@gmail.com>
2016-10-05 02:15:13 -0600

    removed Photo model from pull

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927210140) do
-
+ActiveRecord::Schema.define(version: 20161003204622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,14 +46,6 @@ ActiveRecord::Schema.define(version: 20160927210140) do
     t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
   end
 
-  create_table "photos", force: :cascade do |t|
-    t.string   "title"
-    t.string   "image"
-    t.integer  "bytes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "profiles", force: :cascade do |t|
     t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
@@ -76,11 +67,15 @@ ActiveRecord::Schema.define(version: 20160927210140) do
   end
 
   create_table "spots", force: :cascade do |t|
-    t.string   "name",                   null: false
-    t.text     "description",            null: false
-    t.integer  "user_id",                null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",                      null: false
+    t.text     "description",               null: false
+    t.integer  "user_id",                   null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.string   "spot_picture_file_name"
+    t.string   "spot_picture_content_type"
+    t.integer  "spot_picture_file_size"
+    t.datetime "spot_picture_updated_at"
     t.string   "category"
     t.integer  "difficulty"
     t.text     "difficulty_description"
