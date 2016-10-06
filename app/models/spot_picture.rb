c2d2ba3d0b5e83321276b1e406b7076adacdeaef
Carrie Estes <my4countrygirl@gmail.com>
2016-10-06 16:50:49 -0600

    more bugs fixed

--- app/models/spot_picture.rb
+++ app/models/spot_picture.rb
@@ -2,6 +2,6 @@ class SpotPicture < ActiveRecord::Base
   belongs_to :user
   belongs_to :spot
 
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: 'http://i.imgur.com/czqxX7d.jpg'
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }#, default: TODO
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
