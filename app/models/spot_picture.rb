3b067230c6a39f2675a705ad7d5cec547d5aebb7
Carrie Estes <my4countrygirl@gmail.com>
2016-10-07 13:55:03 -0600

    default for picture

--- app/models/spot_picture.rb
+++ app/models/spot_picture.rb
@@ -2,7 +2,7 @@ class SpotPicture < ActiveRecord::Base
   belongs_to :user
   belongs_to :spot
 
-  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ActionController::Base.helpers.asset_path('SlickSpots.png')
+  has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: 'SlickSpots_:style.png'
 
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
